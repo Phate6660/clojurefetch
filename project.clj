9ab419d4b0f3b9cbb88a792e9e54d90af8258cbd
DrownNotably <61554232+DrownNotably@users.noreply.github.com>
2020-08-30 23:13:57 +0100

    Updated clojure to 1.10.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "I have a problem, ok. I know I do. This is yet another *fetch program, this time in clojure."
   :url "https://github.com/Phate6660/clojurefetch"
   :license {:name "Unlicense"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot clojurefetch.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
