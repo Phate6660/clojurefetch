d37ba594626689f13d598c26e7d2320875a667a4
Ash <valleyknight@protonmail.com>
2020-08-31 17:03:07 -0400

    add uptime field

--- src/clojurefetch/core.clj
+++ src/clojurefetch/core.clj
@@ -11,11 +11,27 @@
 
 (def trim-and-slurp (comp str/trim-newline slurp))
 
+(defn Uptime []
+  (let [uptime_raw (str (trim-and-slurp (java.io.FileReader. "/proc/uptime")))
+        uptime_vector (str/split uptime_raw #"\.")
+        uptime (Integer. (first uptime_vector))
+        days (if (> uptime 86400)
+               (str (int (/ uptime 60 60 24)) "d")
+               "")
+        hours (if (> uptime 3600)
+                (str (int (rem (/ uptime 60 60) 24)) "h")
+                "")
+        minutes (if (> uptime 60)
+                  (str (int (rem (/ uptime 60) 60)) "m")
+                  "")]
+  (str/trim (str days " " hours " " minutes))))
+
 (defn display-help []
   (println "d     display distro
 h     display hostname
 k     display kernel
-u     display user
+U     display user
+u     display uptime
 
 help  display help"))
 
@@ -30,5 +46,7 @@ help  display help"))
           (println (str "Hostname:  " (trim-and-slurp "/etc/hostname"))))
         (when (str/includes? cargs "k")
           (println (str "Kernel:    " (trim-and-slurp "/proc/sys/kernel/osrelease"))))
+        (when (str/includes? cargs "U")
+          (println (str "User:      " (System/getenv "USER"))))
         (when (str/includes? cargs "u")
-          (println (str "User:      " (System/getenv "USER"))))))))
+          (println (str "Uptime:    " (Uptime))))))))
