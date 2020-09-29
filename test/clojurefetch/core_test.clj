905ab93a106096b1042d6b3baa0a9f23c477cd0f
DrownNotably <61554232+DrownNotably@users.noreply.github.com>
2020-09-29 02:12:31 +0100

    Assertions split to multiple tests + some more asserts for min boundary.

--- test/clojurefetch/core_test.clj
+++ test/clojurefetch/core_test.clj
@@ -2,13 +2,27 @@
   (:require [clojure.test :refer :all]
             [clojurefetch.core :refer :all]))
 
-(deftest uptime-test
-  (testing "Testing uptime->string:"
-    (testing "uptimes less than 1 minute"
+(deftest uptime-<min-test
+  (testing "Uptimes less than 1 minute"
       (is (= "" (uptime->string 0)))
       (is (= "" (uptime->string 20)))
       (is (= "" (uptime->string 45)))
-      (is (= "" (uptime->string 59))))
-    (testing "uptimes on the minute boundary"
-      (is (= "1m" (uptime->string 60)))
-      (is (= "2m" (uptime->string 120))))))
+      (is (= "" (uptime->string 59)))))
+
+(deftest uptime-min-boundary-test
+  (testing "Uptimes on the minute boundary"
+    (is (= "1m" (uptime->string 60)))
+    (is (= "1m" (uptime->string 67)))
+    (is (= "1m" (uptime->string 90)))
+    (is (= "2m" (uptime->string 120)))
+    (is (= "3m" (uptime->string 180)))
+    (is (= "14m" (uptime->string 899)))
+    (is (= "15m" (uptime->string 900)))))
+
+(deftest uptime-hour-boundary-test
+  (testing "uptimes on the hour boundary"
+    (is (= "1h" (uptime->string 3600)))))
+
+(deftest uptime-day-boundary-test
+  (testing "uptimes on the day boundary"
+    (is (= "1d" (uptime->string 86400)))))
