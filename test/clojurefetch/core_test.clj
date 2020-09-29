af486695347859aec1068cefa306774cef9c8ce6
DrownNotably <61554232+DrownNotably@users.noreply.github.com>
2020-09-29 01:57:06 +0100

    Started adding tests for uptime.

--- test/clojurefetch/core_test.clj
+++ test/clojurefetch/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [clojurefetch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest uptime-test
+  (testing "Testing uptime->string:"
+    (testing "uptimes less than 1 minute"
+      (is (= "" (uptime->string 0)))
+      (is (= "" (uptime->string 20)))
+      (is (= "" (uptime->string 45)))
+      (is (= "" (uptime->string 59))))
+    (testing "uptimes on the minute boundary"
+      (is (= "1m" (uptime->string 60)))
+      (is (= "2m" (uptime->string 120))))))
